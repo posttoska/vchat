b931698cb8551563ac3e6b974f79e46fc0dcf8b1
posttoska <posttoska@gmail.com>
2025-10-13 23:43:07 +0300

    button handlers and ai summary addded

--- frontend/src/components/RecordMessage.tsx
+++ frontend/src/components/RecordMessage.tsx
@@ -1,3 +1,4 @@
+import { useState, useRef  } from "react";
 import { ReactMediaRecorder } from "react-media-recorder";
 
 type Props = {
@@ -6,14 +7,52 @@ type Props = {
 
 function RecordMessage({ handleStop }: Props) {
 
-    // long click handle
+    // wait stable press
+    const TIME_TO_WAIT = 500;
+    const isUpped = useRef<boolean>(false)
+
+    // helper function to set isUpped
+    function setIsUpped(ref: React.MutableRefObject<boolean>, valueToSet: boolean) {
+        // set value
+        ref.current = valueToSet;
+    };
+    
+    // long click handle when start recording
     function handleOnMouseDown(start: () => void) {
+        // set flag to upped
+        setIsUpped(isUpped, true);
 
+
+        // wait 500 ms anyway
+        setTimeout(() => {
+            // check if button was untouched
+            if (isUpped.current) {
+                // start recoeding
+                start();
+                // set flag to lower
+                setIsUpped(isUpped, false);
+                console.log("start recording...");
+            };
+        
+        // set timeout to 500 ms
+        } , TIME_TO_WAIT)
+
+        
     };
 
-    function handleOnMouseUp (start: () => void) {
+    // long click handle when stop recording
+    function handleOnMouseUp (stop: () => void) {
+        // if the flag was lowered (recording started) then stop it
+        if (!isUpped.current) {
+            stop();
+            console.log("stop recording...");
+        }
 
+        // set flag to lowered anyway
+        setIsUpped(isUpped, false);
     };
+
+    
 
     return (
         // record message with react functions
